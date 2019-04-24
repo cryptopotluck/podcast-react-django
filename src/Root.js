411abc2909363bdf642b5d24ea77c71217147dfc
cryptopotluck <cryptopotluck@gmail.com>
2019-04-24 02:03:20 -0500

    built out most of the backend, graphql is finished mainly working on the react section of the app

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,55 @@
 import React from "react";
-import withRoot from "./withRoot";
+import {Query} from "react-apollo";
 
-const Root = () => <div>Root</div>;
+import {gql} from 'apollo-boost'
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import withRoot from "./withRoot";
+import App from './pages/App';
+import Profile from './pages/Profile';
+import Header from './components/Shared/Header';
+
+
+const Root = () => (
+    <Query query={ME_QUERY}>
+        {({data, loading, error}) => {
+            if (loading) return <div>Loading</div>
+            if (error) return <div>Error</div>
+            const currentUser = data.me;
+
+            return (
+                <Router>
+                    <>
+                        <Header currentUser={currentUser}/>
+                    <Switch>
+                    <Route exact path="/" component={App}/>
+                    <Route path="/profile/:id" component={Profile}/>
+                    </Switch>
+                        </>
+                </Router>
+            )
+        }}
+    </Query>
+);
+
+const ME_QUERY = gql`
+{
+    me {
+    id
+    username
+    email
+    }
+}
+`;
+
+// const GET_TRACKS_QUERY = gql`
+// {
+//     tracks {
+//         id
+//         title
+//         description
+//         url
+//         }
+// }
+// `;
 
 export default withRoot(Root);
